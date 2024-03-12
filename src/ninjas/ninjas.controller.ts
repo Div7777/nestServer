e1c0f86bfe75b78fa415e600f77365f9467b8868
unknown <diveshkumarbhoi@gmail.com>
2024-03-12 17:06:51 +0530

    set controllers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  //Get /ninjas?type=fase --> []
+  @Get()
+  getNinja(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  //Get /ninjas/:id--> { .. }
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+  //Post /ninjas/
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return { name: createNinjaDto.name };
+  }
+
+  //Put/ninjas/:id --> { ... }
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return { id, name: updateNinjaDto };
+  }
+  //Delete /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return { id };
+  }
+}
